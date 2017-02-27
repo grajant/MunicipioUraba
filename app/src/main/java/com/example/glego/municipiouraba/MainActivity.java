0f3d8641e1b881e38a185e0dc6821246a2788692
grajant <glego40_@hotmail.com>
2017-02-27 17:25:11 -0500

    Update_V1

--- app/src/main/java/com/example/glego/municipiouraba/MainActivity.java
+++ app/src/main/java/com/example/glego/municipiouraba/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.glego.municipiouraba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Interpolator;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()){
+            case R.id.menu_profile:
+                intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.logout_menu:
+                break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/glego/municipiouraba/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.glego.municipiouraba;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by practi13 on 27/02/17.
+ */
+
+public class ProfileActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
