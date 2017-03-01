9affe839fc1105808edc8b1b7906c0ee9f61ee9f
grajant <glego40_@hotmail.com>
2017-03-01 18:23:48 -0500

    Update_Code

--- app/src/main/java/com/example/glego/municipiouraba/LoginActivity.java
+++ app/src/main/java/com/example/glego/municipiouraba/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.glego.municipiouraba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void registerClicked(View view) {
+        Intent intent =  new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void loginClicked(View view) {
+        Intent intent =  new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/glego/municipiouraba/RegisterActivity.java
+++ app/src/main/java/com/example/glego/municipiouraba/RegisterActivity.java
@@ -1,13 +1,35 @@
 package com.example.glego.municipiouraba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //private EditText[4] dataText;
+    //private String[4] message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        /*dataText[0] = (EditText) findViewById(R.id.username);
+        dataText[1] = (EditText) findViewById(R.id.password);
+        dataText[2] = (EditText) findViewById(R.id.repassword);
+        dataText[3] = (EditText) findViewById(R.id.email);*/
+
+        //message[0] = getString()
+    }
+
+    public void registerClicked(View view) {
+        for (int i = 0; i < 4; i++){
+
+        }
+    }
+
+    public void cancelClicked(View view) {
     }
 }
